334bf56acccb202b745ca2be77468b7edff4a08e
jodiezhuu <jodiezhu12@gmail.com>
2022-01-01 17:25:33 -0500

    update personal info

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -9,13 +9,13 @@ export default function about() {
      <h1 className='about'>ABOUT</h1>
     <h1 className='header'>MY LIFE</h1>
    <h3 className='header2'>Learn about my aspirations, hobbies, and the next chapter of my life.</h3>
-   <img className='descriptionphoto' src={process.env.PUBLIC_URL + "/images/aboutme.jpg"} alt="DescriptionPhoto"></img>
+   <img className='descriptionphoto' src={process.env.PUBLIC_URL + "/images/IMG_6337.jpg"} alt="DescriptionPhoto"></img>
         <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
-    <p className='description'>Nice to meet you guys! I'm currently in my final year of the International Baccalaureate program and looking to pursue a degree in Computer Science and Finance! With digital news exponentially rising in social media, I want to learn how to develop effective AI solutions and use data curation to analyze information validity. Everywhere I go, I wish to learn from my workspace and critically engage with my peers. I'm highly ambitious and love to jump at any opportunity I can find!</p>
-    <p className='description1'>If I'm not locked in my office, I spend my pastime playing chess. Even though I only began playing a few months ago, I found myself obsessed with this game and the problem-solving skills developed through playing. Feel free to add me on <strong className='description2'>chess.com:jodiezhuu!</strong> Some other hobbies include listening to music, playing volleyball, and doing pilates in my backyard!</p>
+    <p className='description'>Nice to meet you guys! I'm a first year student at University of Waterloo for Computer Science and Finance. With digital news exponentially rising in social media, I want to learn how to develop effective AI solutions and use data curation to analyze information validity. Everywhere I go, I wish to learn from my workspace and critically engage with my peers. I'm highly ambitious and love to jump at any opportunity I can find!</p>
+    <p className='description1'> Some other hobbies include listening to music, playing volleyball, and doing pilates in my backyard!</p>
    
     
 
     <Footer />
-    </>);
+    </>)
 }
--- src/components/pages/Experience.js
+++ src/components/pages/Experience.js
@@ -12,22 +12,22 @@ export default function Experience() {
         
     <div className="format">
         <div className='column1'>
-            <h3 className='lawfirm'>JonesCo. IP Law Firm</h3>
-            <h4 className='jobtitle'>Office Clerk</h4>
-            <h5 className='datetime'>Toronto, ON</h5>
-            <h5 className='datetime'>July 2019-September 2019</h5>
-                <li className='lawfirmdesc'>Inputted contact information of international lawyers using Microsoft OneNote to increase networking efficiency in the firm;</li>
-                <li className='lawfirmdesc'>Used business cards to build a personal profile of 500+ lawyers into Microsoft Sharepoint and Outlook;</li>
-                <li className='lawfirmdesc'>Organized 900+ printed emails involving patents, licensing, trademark law, franchising, and trade secret projects into alphabetical and chronological order.</li>
+        <h3 className='lawfirm'>JonesCo. IP Law Firm</h3>
+        <h4 className='jobtitle'>Office Clerk</h4>
+        <h5 className='datetime'>Toronto, ON</h5>
+        <h5 className='datetime'>July 2019-September 2019</h5>
+            <li className='lawfirmdesc'>Inputted contact information of international lawyers using Microsoft OneNote to increase networking efficiency in the firm;</li>
+            <li className='lawfirmdesc'>Used business cards to build a personal profile of 500+ lawyers into Microsoft Sharepoint and Outlook;</li>
+            <li className='lawfirmdesc'>Organized 900+ printed emails involving patents, licensing, trademark law, franchising, and trade secret projects into alphabetical and chronological order.</li>
         </div>
-        <div className='column2'>
-            <h3 className='lawfirm'>Yale Young Global Scholars</h3>
-            <h4 className='jobtitle'>Student</h4>
-            <h5 className='datetime'>New Haven, CT</h5>
-            <h5 className='datetime'>July 2019</h5>
-                <li className='yygsdesc'>Attended a two-week Politics, Law, and Economics enrichment program for outstanding high school students from over 130 countries;</li>
-                <li className='yygsdesc'>Created and presented a capstone project on Abortion Laws in the 21st Century for professors at Yale University;</li>
-                <li className='yygsdesc'>Received the Yale Young Global Scholars Certificate of Completion.</li>
+    <div className='column2'>
+        <h3 className='lawfirm'>Yale Young Global Scholars</h3>
+        <h4 className='jobtitle'>Student</h4>
+        <h5 className='datetime'>New Haven, CT</h5>
+        <h5 className='datetime'>July 2019</h5>
+            <li className='yygsdesc'>Attended a two-week Politics, Law, and Economics enrichment program for outstanding high school students from over 130 countries;</li>
+            <li className='yygsdesc'>Created and presented a capstone project on Abortion Laws in the 21st Century for professors at Yale University;</li>
+            <li className='yygsdesc'>Received the Yale Young Global Scholars Certificate of Completion.</li>
         </div>
     </div>
 
